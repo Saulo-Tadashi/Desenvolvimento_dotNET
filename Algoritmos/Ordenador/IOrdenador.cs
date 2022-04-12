9179ff42d465a73e2d96b4940a03c75ff9e54ec8
Saulo-Tadashi <tadashi.iguei@gmail.com>
2022-04-11 22:25:39 -0300

    Desenvolvimento de Ordenaçao Criança do branch para desenvolvimento de Algoritmo de Ordenaçao BubbleSort

---
+++ Algoritmos/Ordenador/IOrdenador.cs
@@ -0,0 +1,9 @@
+namespace Algoritmos.Ordenador
+{
+    public interface IOrdenador
+    {
+        public bool ordenar_array (ref int[] ar);   
+
+        public void imprimir_array (ref int[] ar);
+    }
+}
---
+++ Algoritmos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algoritmos
+{
+    class Program
+    {
+        public static void Main (string[] arg)
+        {
+            System.Console.WriteLine("Hellow World!");
+        }
+    }
+}
